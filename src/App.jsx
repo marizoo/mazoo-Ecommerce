3016d4891b39231e466b2cc1a7788c716787055d
marizoo <marizoo@meow.woof>
2022-01-17 23:40:34 +0700

    initial start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      what is upp gorg?
+    </div>
+  )
+}
+
+export default App
